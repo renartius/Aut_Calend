31ef9ebc3ef4dbc5fc1bd8578ddbdd094952e49d
Shiroh-777 <shinji_ikari_07@hotmail.com>
2024-10-11 20:26:34 -0300

    Update views.py

--- PI2/autttt/views.py
+++ PI2/autttt/views.py
@@ -4,6 +4,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+from django.shortcuts import render, redirect
+from .models import Tarefa
+from .forms import TarefaForm
+from django.utils import timezone
+
+def calendario(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('calendario')
+    else:
+        form = TarefaForm()
+
+    tarefas = Tarefa.objects.all()
+    return render(request, 'tarefas/calendario.html', {'form': form, 'tarefas': tarefas})
+
+
 def home(request):
 
     return HttpResponse('Calendário TEA')
