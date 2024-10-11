2fccff3e88854eb1bde6ca3273d33e69abeca27d
Shiroh-777 <shinji_ikari_07@hotmail.com>
2024-10-11 20:17:57 -0300

    Update models.py

--- PI2/autttt/models.py
+++ PI2/autttt/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.titulo
