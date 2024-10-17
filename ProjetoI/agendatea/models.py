6b29c361d3ff9fe25085ba32016382068ee28655
Shiroh-777 <shinji_ikari_07@hotmail.com>
2024-10-17 18:30:00 -0300

    Update models.py

--- ProjetoI/agendatea/models.py
+++ ProjetoI/agendatea/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Compromisso(models.Model):
+    data = models.DateField()
+    hora = models.TimeField()
+    local = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    pessoa = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.tipo} com {self.pessoa} em {self.local} no dia {self.data} às {self.hora}"
